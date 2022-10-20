29c1e0f2760c47afb684699264aee3c7d335ccd1
Stealr <baranovm003@yandex.ru>
2022-10-20 14:54:56 +0300

    Registration

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import requests
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 import psycopg2
 
 app = Flask(__name__)
@@ -23,19 +23,31 @@ def index():
     return render_template('login.html')
 
 
-@app.route('/login/', methods=['POST'])
+@app.route('/login/', methods=['POST', 'GET'])
 def login():
-    username = request.form.get('username')
-    password = request.form.get('password')
-    if username == '' or password == '':
-        return render_template('login_errempty.html')  # шаблон если поля пустые
-    cursor.execute('SELECT * FROM service.users WHERE login=%s AND password=%s',
-                   (str(username), str(password)))
-    records = list(cursor.fetchall())
-    if len(records) != 0:  # смотрим есть ли данные о пользователе с данным логином и паролем.
-        return render_template('account.html', full_name=records[0][1], username=username, password=password)
-    else:
-        return render_template('login_errlap.html')  # шаблон если пользователь не найден
+    if request.method == 'POST':
+        if request.form.get("login"):
+            username = request.form.get('username')
+            password = request.form.get('password')
+            cursor.execute("SELECT * FROM service.users WHERE login=%s AND password=%s", (str(username), str(password)))
+            records = list(cursor.fetchall())
+            return render_template('account.html', full_name=records[0][1])
+        elif request.form.get("registration"):
+            return redirect("/registration/")
+    return render_template('login.html')
+
+
+@app.route('/registration/', methods=['POST', 'GET'])
+def registration():
+    if request.method == 'POST':
+        name = request.form.get('name')
+        login = request.form.get('login')
+        password = request.form.get('password')
+        cursor.execute('INSERT INTO service.users (full_name, login, password) VALUES(%s, %s, %s);',
+                       (str(name), str(login), str(password)))
+        conn.commit()
+        return redirect('/login/')
+    return render_template('registration.html')
 
 
 app.run(debug=True)
