518a7f6c7bde3b4257d7bf215bde405c418906cb
Stealr <baranovm003@yandex.ru>
2022-10-23 19:57:16 +0300

    Registration_v.1.00

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import logging
+
 import requests
 from flask import Flask, render_template, request, redirect
 import psycopg2
@@ -23,7 +25,7 @@ def index():
     return render_template('login.html')
 
 
-@app.route('/login/', methods=['POST', 'GET'])
+@app.route('/login/', methods=['POST'])
 def login():
     if request.method == 'POST':
         if request.form.get("login"):
@@ -34,8 +36,7 @@ def login():
             cursor.execute("SELECT * FROM service.users WHERE login=%s AND password=%s", (str(username), str(password)))
             records = list(cursor.fetchall())
             if len(records) != 0:  # смотрим есть ли данные о пользователе с данным логином и паролем
-                print(records)
-                return render_template('account.html', full_name=records[0][1])
+                return render_template('account.html', full_name=records[0][1], username=username, password=password)
             else:
                 return render_template('login_errlap.html')  # шаблон если пользователь не найден
         elif request.form.get("registration"):
@@ -45,15 +46,25 @@ def login():
 
 @app.route('/registration/', methods=['POST', 'GET'])
 def registration():
-    if request.method == 'POST':
-        name = request.form.get('name')
-        login = request.form.get('login')
-        password = request.form.get('password')
-        cursor.execute('INSERT INTO service.users (full_name, login, password) VALUES(%s, %s, %s);',
-                       (str(name), str(login), str(password)))
-        conn.commit()
-        return redirect('/login/')
-    return render_template('registration.html')
+    # try:  # Если существует такой же логин в бд, то перекидывает в expect.
+        if request.method == 'POST':
+            name = request.form.get('name')
+            login = request.form.get('login')
+            password = request.form.get('password')
+            if name == '' or login == '' or password == '':
+                return render_template('registration_errempty.html')
+            cursor.execute("SELECT * FROM service.users WHERE login=\'%s\'" % (str(login)))
+            records = list(cursor.fetchall())
+            if len(records) == 0: # Проверяем нашлось ли совпадение
+                cursor.execute('INSERT INTO service.users (full_name, login, password) VALUES(%s, %s, %s);',
+                               (str(name), str(login), str(password)))
+                conn.commit()
+                return redirect('/login/')
+            else:
+                return render_template('registration_errlog.html')
+        return render_template('registration.html')
+    # except:
+    #     return render_template('registration_errlog.html')
 
 
 app.run(debug=True)
